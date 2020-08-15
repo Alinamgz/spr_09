85dda3e452489ad1867c04bc7baeea95ef2bf431
Alina Mazur <amazur@e1r3p14.unit.ua>
2020-08-15 15:45:10 +0300

    t01

---
+++ t01/macros.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define MX_SUM(x, y) ((x) + (y))
+#define MX_MULT(x, y) ((x) * (y))
+#define MX_MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MX_ABS(x) ((x) > 0 ? (x) : ((x) * -1))
+#define MX_IS_ODD(x) (((x) % 2 == 1) ? 1 : 0)
